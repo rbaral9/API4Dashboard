42e5a1f82b71762a2be32c4f13f3ccf6d34d3de4
Rashmi Baral <rashmibaral@live.com>
2024-03-27 11:31:39 -0700

    this is first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PolicyStats.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
